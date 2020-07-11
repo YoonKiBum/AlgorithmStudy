9e0b1da394e20f6def3c19497da1a7d1b1540225
ykbsuper <62062154+ykbsuper@users.noreply.github.com>
2020-07-11 12:10:33 +0900

    1700.cpp Second

--- Greedy/1700.cpp
+++ Greedy/1700.cpp
@@ -44,7 +44,7 @@ int main(void)
 				plug[cnt] = seq[i];
 				cnt++;
 			}
-			else // 비어 있는 코드가 없고 이미 꽂혀있는 코드가 아닌경우 가장 나중에 사용되거나 앞으로 사용이 되지 않을 기계를 찾음
+			else // 비어 있는 코드가 없고 이미 꽂혀있는 코드가 아닌경우
 			{
 				int idx = 0;
 				int device = -1;
@@ -52,27 +52,26 @@ int main(void)
 				for (int j = 0; j < N; j++)
 				{
 					int last = 0;
-					
-					for (int l = i + 1; l < K; l++)
+					for (int l = i + 1; l < K; l++) // 꽂아야 할 기계의 다음부터 끝까지 조회하며 가장 최소로 뽑을 수 있는 기계를 찾아서 뽑음
 					{
-						if (plug[j] == seq[l])
+						if (plug[j] == seq[l]) // 조회한게 꽂혀 있는 경우
 						{
 							break;
 						}
-						last++;
+						last++; // 조회한게 꽂혀 잊지 않은 경우 하나 증가시킴
 					}
-					if (last > device)
+					if (last > device) // last가 device보다 크다면 
 					{
-						idx = j;
-						device = last;
+						idx = j; // idx를 j로 재정의
+						device = last; // device에 last를 넣음
 					}
 				}
-				cnt2++;
-				plug[idx] = seq[i];
+				cnt2++; // cnt2를 하나 증가시킴
+				plug[idx] = seq[i]; // 뽑아야 할 위치에 새로 추가함
 			}
 		}
 	}
-	cout << cnt2;
+	cout << cnt2; // cnt2 출력
 	return 0;
 }
 
