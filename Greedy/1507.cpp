6a8ea6dec191b947977e4f6f49698603c43b3f28
ykbsuper <62062154+ykbsuper@users.noreply.github.com>
2020-07-14 22:50:18 +0900

    1507.cpp

--- Greedy/1507.cpp
+++ Greedy/1507.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#define MAX 20
+#define MAX 21
 
 using namespace std;
 
 int arr[MAX][MAX] = { 0 }; // 도시와 도시 사이의 시간을 담을 배열
 int arr2[MAX][MAX] = { 0 }; // 복사본
+
 void floydWarshall(int n)
 {
 	int total = 0;
 	
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) // 입력받은 배열 arr[i][j]를 arr2[i][j]에 넣어준다
 	{
 		for (int j = 0; j < n; j++)
 		{
@@ -17,22 +18,22 @@ void floydWarshall(int n)
 		}
 	}
 
-	for (int k = 0; k < n; k++)
+	for (int k = 0; k < n; k++) 
 	{
 		for (int i = 0; i < n; i++)
 		{
 			for (int j = 0; j < n; j++)
 			{
-				if (i == j || j == k || i == k)
+				if (i == j || j == k || i == k) // 이 경우는 그냥 넘김
 				{
 					continue;
 				}
-				else if (arr[i][k] + arr[k][j] < arr[i][j])
+				else if (arr[i][k] + arr[k][j] < arr[i][j]) // i 에서 j를 갈때 k를 거쳐가는 경우가 더 작은 경우 즉 주어진 값들이 변경이 되는 경우 -1 반환
 				{
 					cout << -1;
 					return;
 				}
-				else if (arr[i][k] + arr[k][j] == arr[i][j])
+				else if (arr[i][k] + arr[k][j] == arr[i][j]) // i에서 j를 갈때 k를 거쳐가는 경우와 같다면 0으로 바꿈 
 				{
 					arr2[i][j] = 0;
 				}
