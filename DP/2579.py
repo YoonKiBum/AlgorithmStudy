b0f7fe63959d0d1bf0b84d233289a376ac9a29b8
KiBumYoon <62062154+YoonKiBum@users.noreply.github.com>
2020-10-09 15:29:37 +0900

    Create 2579.py

---
+++ DP/2579.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = [0] * 301
+for i in range(n):
+  data[i] = int(input())
+
+d = [0] * 301
+d[0] = data[0]
+d[1] = data[0] + data[1]
+d[2] = max(data[0] + data[2], data[1] + data[2])
+
+for i in range(3, n):
+  d[i] = max(data[i] + data[i-1] + d[i-3], data[i] + d[i-2])
+
+print(d[n-1])
