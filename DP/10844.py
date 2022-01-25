669c1a01a4849422d2c5400571d6c6a995c4144d
YoonKiBum <ykbsuper@naver.com>
2022-01-25 18:46:08 +0900

    modify 10844.py

--- DP/10844.py
+++ DP/10844.py
@@ -1,13 +1,16 @@
-N = int(input())
-data = [[1] * 10 for _ in range(N)]
+n = int(input())
+data = [[0] * 10 for _ in range(n)]
 
-for i in range(1, N):
-  for j in range(0, 10): 
+for i in range(1, 10):
+  data[0][i] = 1
+
+for i in range(1, n):
+  for j in range(10):
     if 1 <= j <= 8:
       data[i][j] = data[i-1][j-1] + data[i-1][j+1]
-    elif j == 9:
-      data[i][j] = data[i-1][j-1]
     elif j == 0:
       data[i][j] = data[i-1][j+1]
+    elif j == 9:
+      data[i][j] = data[i-1][j-1]
 
-print(sum(data[N-1][1:])%1000000000)
+print(sum(data[n-1][0:])%1000000000)
